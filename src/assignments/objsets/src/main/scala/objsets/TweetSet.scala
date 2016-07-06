0380b55abebf7565d39e723569ab6c344509d0e4
pietro909 <pietro.grandi.3d@gmail.com>
2016-07-06 10:39:59 +0200

    union implemented, filter just for empty -_-

--- src/assignments/objsets/src/main/scala/objsets/TweetSet.scala
+++ src/assignments/objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,8 +54,8 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
-  
+    def union(that: TweetSet): TweetSet
+
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
@@ -107,8 +107,10 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = new Empty
+
+    def union(that: TweetSet): TweetSet = that
+
   /**
    * The following methods are already implemented
    */
@@ -124,9 +126,15 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet =
+      if (p(elem))
+        left.filterAcc(p, new NonEmpty(elem, new Empty, new Empty)) union right.filterAcc(p, new Empty)
+      else
+        left.filterAcc(p, new Empty) union right.filterAcc(p, new Empty)
+
+    def union(that: TweetSet): TweetSet =
+      ((left union right) union that) incl elem
+
   /**
    * The following methods are already implemented
    */
