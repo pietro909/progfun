9c0c9abaaab4ba134d26c47162338abd0b4a453c
pietro909 <pietro.grandi.3d@gmail.com>
2016-07-11 14:01:15 +0200

    add equal

--- src/week4/ISort.scala
+++ src/week4/ISort.scala
@@ -1,7 +1,7 @@
 
 def insert(x: Int, xs: List[Int]): List[Int] = xs match {
   case List() => List(x)
-  case y :: ys => if (x < y) x :: xs else y :: insert(x, ys)
+  case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
 }
 
 def isort(l: List[Int]): List[Int] = l match {
