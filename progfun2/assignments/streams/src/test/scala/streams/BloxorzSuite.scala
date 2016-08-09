91d434837c26af8f2ec0f49055e05a96d2c8a66f
pietro909 <pietro.grandi.3d@gmail.com>
2016-08-09 09:13:04 +0200

    custom tests ok!

--- progfun2/assignments/streams/src/test/scala/streams/BloxorzSuite.scala
+++ progfun2/assignments/streams/src/test/scala/streams/BloxorzSuite.scala
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
 import Bloxorz._
-import GameDef.Left
 
 @RunWith(classOf[JUnitRunner])
 class BloxorzSuite extends FunSuite {
@@ -73,22 +72,23 @@ class BloxorzSuite extends FunSuite {
       assert(result.toSet == expected)
     }
   }
-/*
-  test("newNeighborsOnly level 1") {
-    val result = newNeighborsOnly(
-      Set(
-        (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
-        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
-      ).toStream,
 
-    Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
-  )
-    val expected = Set(
-      (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
-    ).toStream
-    assert(result.toSet == expected)
+  test("newNeighborsOnly level 1") {
+    new Level1 {
+      val result = newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+
+      Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+    )
+      val expected = Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      )
+      assert(result.toSet == expected)
+    }
   }
-*/
 
 	test("optimal solution for level 1") {
     new Level1 {
