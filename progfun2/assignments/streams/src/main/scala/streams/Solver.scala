30c28cf8aae6e9ec7efb94619157039dbbaf82a1
pietro909 <pietro.grandi.3d@gmail.com>
2016-08-09 19:13:14 +0200

    YEAHgit status!

--- progfun2/assignments/streams/src/main/scala/streams/Solver.scala
+++ progfun2/assignments/streams/src/main/scala/streams/Solver.scala
@@ -11,7 +11,8 @@ trait Solver extends GameDef {
    * Returns `true` if the block `b` is at the final position
    */
   def done(b: Block): Boolean =
-    b.isStanding && b.b1.row == goal.row && b.b1.col == goal.col
+    (b.b1.row == goal.row && b.b1.col == goal.col) &&
+    (b.b2.row == goal.row && b.b2.col == goal.col)
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -67,18 +68,45 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] =
+    if (initial.isEmpty) Stream.empty
+    else {
+      // val (block, moves) = initial.head
+      // val nwh = neighborsWithHistory(block, moves)
+      // println(s"NWH")
+      // nwh.toList map (println)
+      val more = (for {
+          (block, moves) <- initial
+          paths <- newNeighborsOnly(neighborsWithHistory(block, moves), explored)
+          //paths <- newNeighborsOnly(nwh, explored)
+//        paths <- neighborsWithHistory(block, moves)
+      } yield paths).toStream
+//      println(s"PATHS")
+      more.toList map (println)
+      //println("EXPLORED")
+      //println(explored)
+      more ++ from(more, explored ++ more.map(_._1).toSet)
+    }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = {
+    val r = from(Stream((startBlock, List())), Set(startBlock))
+    //println(s"pfs: $r")
+    //r.toList map (println)
+    r
+  }
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] =
+    pathsFromStart filter { case (block, moves) => {
+      // println(s"block => $block - done? ${done(block)}")
+      done(block)
+    }}
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -88,5 +116,10 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal.headOption match { 
+    case None => List()
+    case Some((block, moves)) =>
+//      println(moves)
+      moves.reverse
+  }
 }
--- progfun2/assignments/streams/src/test/scala/streams/BloxorzSuite.scala
+++ progfun2/assignments/streams/src/test/scala/streams/BloxorzSuite.scala
@@ -40,6 +40,15 @@ class BloxorzSuite extends FunSuite {
     val optsolution = List(Right, Right, Down, Right, Right, Right, Down)
   }
 
+  trait Level0 extends SolutionChecker {
+      /* terrain for level 1*/
+
+    val level =
+    """Sooo
+      |oooT""".stripMargin
+
+  }
+
 
 	test("terrain function level 1") {
     new Level1 {
@@ -90,14 +99,20 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
-	test("optimal solution for level 1") {
+  test("optimal solution for level 0") {
+    new Level0 {
+      println(terrain(Pos(1,3)))
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+  test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
     }
   }
 
-
-	test("optimal solution length for level 1") {
+  test("optimal solution length for level 1") {
     new Level1 {
       assert(solution.length == optsolution.length)
     }
