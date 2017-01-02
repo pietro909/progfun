5b61ed729d226d33402503ef1c17a36d3eff9f08
Pietro Grandi <pietro.grandi@evokegaming.com>
2017-01-02 18:08:57 +0100

    Parallel vertical blur ready

--- parprog1/assignments/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
+++ parprog1/assignments/scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -62,12 +62,11 @@ object VerticalBoxBlur {
    *  columns.
    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
-    // TODO implement using the `task` construct and the `blur` method
     val range = 0 until src.width by numTasks
-    range.zip(range.tail).foreach {
+    range.zip(range.tail).map({
       case (from, end) =>
-        blur(src, dst, from, end - 1, radius)
-    }
+        task { blur(src, dst, from, end - 1, radius) }
+    }).map(task => task.join())
   }
 
 }
